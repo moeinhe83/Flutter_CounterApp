2cb340dda453809567e2088a73a1e7d19fbfe649
moeinhe83 <moeinheshmati1383@gmail.com>
2025-11-24 02:14:23 +0330

    Create StatefulWidget

--- lib/main.dart
+++ lib/main.dart
@@ -47,8 +47,23 @@ class MyApp extends StatelessWidget {
             ),
           ),
         ),
+        body: HomePage(),
       ),
     );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
 
